8690cb26fe85e4ea89c856c8b6c88d1ceadb466b
huxiaoyu <972309778@qq.com>
2019-08-12 12:26:48 +0800

    update readme.md

--- example/gen_proto.sh
+++ example/gen_proto.sh
@@ -6,6 +6,6 @@ PACKAGE_NAME=pack
 
 echo '开始'
 
-./gene_proto -toFileSrc $TO_FILE -src $SRC_FILE -packageName $PACKAGE_NAME
+gotoproto -toFileSrc $TO_FILE -src $SRC_FILE -packageName $PACKAGE_NAME
 
 echo '完成'
